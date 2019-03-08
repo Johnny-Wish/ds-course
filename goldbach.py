dced6c2ab78f0ec6d7b89e592c3638556469a39c
Shuheng Johnny Liu <wish1104@outlook.com>
2019-03-08 17:16:31 +0800

    implement the goldbach test for integers between 2 and 100

---
+++ goldbach.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+def ceil(x):
+    return int(np.ceil(x))
+
+
+class PrimeGenerator:
+    def __init__(self, n):
+        self.n = n
+        self._primes = None
+
+    def generate_primes(self):
+        self._primes = list(range(2, self.n))
+
+        for i in range(2, self.n):
+            for j in range(2, min(i, ceil(np.sqrt(i)) + 1)):
+                if i % j == 0:
+                    self._primes.pop(self._primes.index(i))
+                    break
+
+    @property
+    def primes(self):
+        if self._primes is None:
+            self.generate_primes()
+        return self._primes
+
+
+class Validator(ABC):
+    def __init__(self):
+        pass
+
+    def validate(self, n_runs, verbose=True):
+        for run in range(n_runs):
+            if not self.argument(run, verbose=verbose):
+                return False
+
+        return True
+
+    @abstractmethod
+    def argument(self, run_id, verbose=True) -> bool:
+        return True
+
+
+class GoldbachValidator(Validator):
+    def map_run_id(self, run_id):
+        return run_id * 2 + 4
+
+    def argument(self, run_id, verbose=True):
+        n = self.map_run_id(run_id)
+        primes = PrimeGenerator(n + 1).primes
+
+        for p in primes:
+            if (n - p) in primes:
+                if verbose:
+                    print("{} = {} + {}".format(n, p, n - p))
+                return True
+
+        if verbose:
+            print("{} cannot be divided as the sum of two primes".format(n))
+        return False
+
+
+if __name__ == '__main__':
+    success = GoldbachValidator().validate(50)
+    if success:
+        print("Argument validated")
+    else:
+        print("Argument disproved")
