a5c9ea515ee59630b4486c64df44a7a438a78767
Shuheng Liu <wish1104@outlook.com>
2019-03-09 00:08:41 +0800

    add doc strings to all methods and functions

--- factorization.py
+++ factorization.py
@@ -4,6 +4,7 @@ from goldbach import PrimeGenerator
 
 class Factorizer:
     def __init__(self, n):
+        """an agent that factorize a given integer"""
         if n <= 0:
             raise ValueError("only positive integers can be passed")
 
@@ -12,6 +13,7 @@ class Factorizer:
         self._factor_count = {}
 
     def factorize(self):
+        """factorize self.n into the product of a sequence of primes"""
         if self.n == 1:
             self._factor_count = {1: 1}
             return
@@ -19,12 +21,13 @@ class Factorizer:
         n = self.n  # avoid changing self.n during factorization
         for p in self._primes:
             while n % p == 0:
-                self.gather_factor(p)
+                self._gather_factor(p)
                 n //= p
 
-        assert self._factor_count, "no factor counted"
+        assert self._factor_count, "no factor counted"  # check internal correctness
 
-    def gather_factor(self, factor):
+    def _gather_factor(self, factor):
+        """register a factor to self._factor_count; increment count by 1 if already registered"""
         if factor in self._factor_count:
             self._factor_count[factor] += 1
         else:
@@ -32,15 +35,18 @@ class Factorizer:
 
     @property
     def factor_count(self):
+        """fetches the factor_count dict"""
         if not self._factor_count:
             self.factorize()
         return self._factor_count
 
     def __str__(self):
+        """printable string representation of self"""
         factor_substring = " * ".join(str(f) for f in self.factor_count for _ in range(self.factor_count[f]))
         return "Factorization of {} = {}".format(self.n, factor_substring)
 
     def __repr__(self):
+        """internal string representation of self"""
         return "Factor Count of {} = {}".format(self.n, self.factor_count)
 
 
--- goldbach.py
+++ goldbach.py
@@ -3,11 +3,16 @@ import numpy as np
 
 
 def ceil(x):
+    """ceil an input and return as an integer"""
     return int(np.ceil(x))
 
 
 class PrimeGenerator:
     def __init__(self, n):
+        """
+        generator of prime numbers less than n
+        :param n: ceil for prime list (excluded)
+        """
         self.n = n
         self._primes = None
 
@@ -22,6 +27,7 @@ class PrimeGenerator:
 
     @property
     def primes(self):
+        """fetch a list of prime numbers under self.n"""
         if self._primes is None:
             self.generate_primes()
         return self._primes
@@ -37,14 +43,29 @@ class Validator(ABC):
         return run_id
 
     def validate(self, n_runs, verbose=True):
+        """
+        validate the statement for n = 1, 2, ..., (n_runs - 1), returns True is all runs pass
+        :param n_runs: number of run
+        :param verbose: verbosity (level) to be passed to self.argument()
+        :return: bool, truth of all statement
+        """
         for run in range(n_runs):
-            if not self.argument(run, verbose=verbose):
+            if self.statement(self.__class__.map_run_id(run), verbose=verbose):
+                if verbose and (run + 1) % 5 == 0 or run == n_runs - 1:
+                    print()
+            else:
                 return False
 
         return True
 
     @abstractmethod
-    def argument(self, run_id, verbose=True) -> bool:
+    def statement(self, k, verbose=True) -> bool:
+        """
+        a statement that is either True or False for some certain k
+        :param k: primary argument of the statement
+        :param verbose: verbosity (level)
+        :return: bool, truth of the statement
+        """
         return True
 
 
@@ -54,24 +75,28 @@ class GoldbachValidator(Validator):
         """map run_id to the primary argument of the statement"""
         return run_id * 2 + 6
 
-    def argument(self, run_id, verbose=True):
-        n = self.map_run_id(run_id)
-        primes = PrimeGenerator(n + 1).primes
+    def statement(self, k, verbose=True):
+        """
+        statement that an even number k can be written as p1 + p2 where p1 and p2 are prime numbers
+        :param verbose: print the primes found
+        :return: bool, value of the statement
+        """
+        primes = PrimeGenerator(k + 1).primes
 
         for p in primes:
-            if (n - p) in primes:
+            if (k - p) in primes:
                 if verbose:
-                    print("{} = {} + {}".format(n, p, n - p), end="\n" if (run_id + 1) % 5 == 0 else " ")
+                    print("{} = {} + {}".format(k, p, k - p), end="\t\t")
                 return True
 
         if verbose:
-            print("{} cannot be divided as the sum of two primes".format(n))
+            print("{} cannot be written as the sum of two primes".format(k))
         return False
 
 
 if __name__ == '__main__':
-    success = GoldbachValidator().validate(50)
+    success = GoldbachValidator().validate(n_runs=48)
     if success:
-        print("Argument validated")
+        print("Goldbach Conjecture Validated")
     else:
-        print("Argument disproved")
+        print("Goldbach Conjecture Disproved")
